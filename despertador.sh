2084da9c4467216fcacd30311c5113b40a31deda
rogtejada <rogtejada@hotmail.com>
2019-06-09 16:40:24 -0300

    add verification of chorme in despertador

--- despertador.sh
+++ despertador.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-##SPIN INTERFACE
+##LITTLE SPIN INTERFACE
 sp='/-\|'
 sc=0
 spin() {
@@ -8,21 +7,31 @@ spin() {
     ((sc==${#sp})) && sc=0
     sleep 0.1
 }
-
 endspin() {
     printf '\r%s\n' "$@"
     sleep 0.1
 }
 
-time_now=$(date +%k%M)
-echo Now is: $time_now
-echo When you want to wake Up? HHmm, with no dots or spaces
-read time_wake_up
+despertador() {
+    time_now=$(date +%k%M)
+    echo Now is: $time_now
+    echo When you want to wake Up? HHmm, with no dots or spaces
+    read time_wake_up
 
-while [ $time_now -ne $time_wake_up ]; do
-  time_now=$(date +%k%M)
-  spin
-done
+    while [ $time_now -ne $time_wake_up ]; do
+      time_now=$(date +%k%M)
+      spin
+    done
 
-endspin
-google-chrome --app=https://www.youtube.com/watch?v=vXq0k07desIe
+    endspin
+    google-chrome --app=https://www.youtube.com/watch?v=vXq0k07desIe
+}
+
+##Verification of chrome
+hasChrome=$(google-chrome --version)
+if echo "$hasChrome" | egrep '*not found*'
+then
+  echo You need to Install Chrome First
+else
+  despertador
+fi
